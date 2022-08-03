e696ac24ea01933225bb0b964f18ee76df4a6626
Navya-26 <60292076+Navya-26@users.noreply.github.com>
2022-08-03 21:01:19 +0530

    Update First_File.ps1

--- First_File.ps1
+++ First_File.ps1
@@ -7,35 +7,33 @@ $Table += $objrecord
 $Record."Role"="6"
 $objRecord = New-Object PSObject -property $Record
 $Table += $objrecord
-
-$Table | export-csv "table_AD_Role.csv" -NoTypeInformation
+$primaryAlias = $env:Release_PrimaryArtifactSourceAlias
+cd $primaryAlias
+cd drop
+$current_date = $(get-date -f yyyy-MM-dd)
+$filename = "table_AD_$($current_date).csv"
+$Table | export-csv $filename -NoTypeInformation
 
 if($env:SYSTEM_PULLREQUEST_SOURCEBRANCH)
 {
-"This is a PR build"
 $branch = ("$env:SYSTEM_PULLREQUEST_SOURCEBRANCH").replace("refs/heads/","")
-Write-Host "Branch name is: " $branch
 }
 elseif($env:BUILD_SOURCEBRANCH)
 {
-"This is a Non-PR build pipeline run"
 $branch = ("$env:BUILD_SOURCEBRANCH").replace("refs/heads/","")
 }
 Else
 {
 Exit
 }
-Write-Host $branch
-# git init
-Write-Host "1"
-ls
+git init
+
 git checkout $branch
 # git pull
-ls
-Write-Host "2"
+
 
 git add .
-git config user.email "navya.26.02.00@gmil.com"
+git config user.email "navya.26.02.00@gmail.com"
 git config user.name "Navya"
-git commit -am "commit message [ci skip]"
+git commit -am "Added $($FileName)"
 git push --set-upstream origin $branch
