0d97c22aec16f3620d8137643f48cade283375ff
Navya-26 <60292076+Navya-26@users.noreply.github.com>
2022-10-13 21:22:00 +0530

    Create Second_File.ps1

---
+++ Second_File.ps1
@@ -0,0 +1,39 @@
+Write-Host "Hi"
+$Table = @()
+$Record = @{"Role" = ""}
+$Record."Role"="33"
+$objRecord = New-Object PSObject -property $Record
+$Table += $objrecord
+$Record."Role"="66"
+$objRecord = New-Object PSObject -property $Record
+$Table += $objrecord
+$primaryAlias = $env:Release_PrimaryArtifactSourceAlias
+cd $primaryAlias
+cd drop
+$current_date = $(get-date -f yyyy-MM-dd)
+$filename = "table_AD_$($current_date).csv"
+$Table | export-csv $filename -NoTypeInformation
+
+if($env:SYSTEM_PULLREQUEST_SOURCEBRANCH)
+{
+$branch = ("$env:SYSTEM_PULLREQUEST_SOURCEBRANCH").replace("refs/heads/","")
+}
+elseif($env:BUILD_SOURCEBRANCH)
+{
+$branch = ("$env:BUILD_SOURCEBRANCH").replace("refs/heads/","")
+}
+Else
+{
+Exit
+}
+git init
+
+git checkout $branch
+# git pull
+
+
+git add .
+git config user.email $env:RELEASE_REQUESTEDFOREMAIL
+git config user.name $env:BUILD_REQUESTEDFOR
+git commit -am "Added $($FileName)"
+git push --set-upstream origin $branch
